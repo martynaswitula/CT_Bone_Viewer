b9339bf195fabdcfa415b638eec06395d70f4986
martynaswitula <martyna.switula@gmail.com>
2026-06-10 21:50:54 +0200

    Update Terraform with API infrastructure

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,30 +13,65 @@ provider "azurerm" {
   tenant_id       = "50c76291-0c80-4444-a2fb-4f8ab168c311"
 }
 
-# Grupa zasobów
 resource "azurerm_resource_group" "ct_viewer" {
   name     = "ct-viewer-rg"
   location = "Switzerland North"
+
+  lifecycle {
+    ignore_changes = [location]
+  }
 }
 
-# Plan App Service
 resource "azurerm_service_plan" "ct_viewer" {
   name                = "ct-viewer-plan"
   resource_group_name = azurerm_resource_group.ct_viewer.name
-  location            = azurerm_resource_group.ct_viewer.location
+  location            = "Switzerland North"
+  os_type             = "Linux"
+  sku_name            = "F1"
+
+  lifecycle {
+    ignore_changes = [location]
+  }
+}
+
+resource "azurerm_linux_web_app" "ct_viewer" {
+  name                = "ct-bone-viewer"
+  resource_group_name = azurerm_resource_group.ct_viewer.name
+  location            = "Switzerland North"
+  service_plan_id     = azurerm_service_plan.ct_viewer.id
+  https_only          = true
+
+  lifecycle {
+    ignore_changes = [location, site_config, app_settings, 
+                      client_affinity_enabled,
+                      ftp_publish_basic_authentication_enabled,
+                      webdeploy_publish_basic_authentication_enabled]
+  }
+
+  site_config {
+    application_stack {
+      php_version = "8.2"
+    }
+  }
+}
+
+resource "azurerm_service_plan" "ct_api" {
+  name                = "ct-api-plan"
+  resource_group_name = azurerm_resource_group.ct_viewer.name
+  location            = "Switzerland North"
   os_type             = "Linux"
   sku_name            = "F1"
 }
 
-# App Service
-resource "azurerm_linux_web_app" "ct_viewer" {
-  name                = "ct-bone-viewer"
+resource "azurerm_linux_web_app" "ct_api" {
+  name                = "ct-bone-api"
   resource_group_name = azurerm_resource_group.ct_viewer.name
-  location            = azurerm_resource_group.ct_viewer.location
-  service_plan_id     = azurerm_service_plan.ct_viewer.id
+  location            = "Switzerland North"
+  service_plan_id     = azurerm_service_plan.ct_api.id
   https_only          = true
 
   site_config {
+    always_on = false
     application_stack {
       php_version = "8.2"
     }
