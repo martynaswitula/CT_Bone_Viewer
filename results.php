6555eb7cb71ea838eda467b43bdadd54e748bd18
martynaswitula <martyna.switula@gmail.com>
2026-06-13 20:49:57 +0200

    Demo: zmiana tytulu strony

--- results.php
+++ results.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['lekarz_id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Historia Badań - CT Bone Viewer</title>
+    <title>Historia Badań - CT Bone Viewer!</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
     <style>
